2ba68f526f9381ad61bceab362c4b4f0337a35ff
Laurence <ldn221@nyu.edu>
2019-11-04 12:41:28 -0800

    Complete react components exercise

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
 // TODO
+var GroceryList = (props) => (
+    <ul>
+       {props.list.map(item =>
+            <GroceryListItem item={item}/>
+        )}
+    </ul>
+);
+
+class GroceryListItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            done: false
+          };
+
+    }
+
+    onListItemClick() {
+        this.setState({
+          done: !this.state.done
+        });
+    }
+
+    render() {
+
+        var style = {
+            textDecoration: this.state.done ? 'line-through' : 'none'
+        };
+
+        return (
+            <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+        )
+    }
+};
+
+
+var list = ['Tomatoes', 'Eggs', 'Milk', 'Chips'];
+ReactDOM.render(<GroceryList list={list}/>, document.getElementById("app"));
+
+
+// var Tomatoes = () => (
+//     <li>Tomatoes</li>
+// );
+
+// var Eggs = () => (
+//     <li>Eggs</li>
+// );
